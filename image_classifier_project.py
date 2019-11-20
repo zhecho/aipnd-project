2e8a9d11be9e11b2d240a22306a5022856ef3bc2
Zhecho <zhechev.zhecho@gmail.com>
2019-11-20 16:51:42 +0200

    - data['testloader'] 	modified:   image_classifier_project.py

--- image_classifier_project.py
+++ image_classifier_project.py
@@ -129,8 +129,8 @@ def train(model, epochs, data, optimizer, criterion, device):
                             
                 print("Epoch: {}/{}.. ".format(e+1, epochs),
                       "Training Loss: {:.3f}.. ".format(running_loss/print_every),
-                      "Test Loss: {:.3f}.. ".format(test_loss/len(testloader)),
-                      "Test Accuracy: {:.3f}".format(accuracy/len(testloader)),
+                      "Test Loss: {:.3f}.. ".format(test_loss/len(data['testloader'])),
+                      "Test Accuracy: {:.3f}".format(accuracy/len(data['testloader'])),
                       "Runtime: {}".format(datetime.now() - start_ts), )
                 
                 running_loss = 0
